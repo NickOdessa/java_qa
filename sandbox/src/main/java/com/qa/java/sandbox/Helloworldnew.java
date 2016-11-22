de5b04e158856fe380455c962eb43bd128af5ce6
NickOdessa <tkach.nickolay@gmail.com>
2016-11-23 00:19:12 +0200

    add function

--- sandbox/src/main/java/com/qa/java/sandbox/Helloworldnew.java
+++ sandbox/src/main/java/com/qa/java/sandbox/Helloworldnew.java
@@ -3,7 +3,25 @@ package com.qa.java.sandbox;
 public class HelloWorldNew {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, world");
+
+	    hello("world");
+      double  l = 5;
+
+      System.out.println("Площадь квадрата со стороной " + l + " = "+ area(l));
+
+      double a=4;
+      double  b=6;
+
+      System.out.println("Площадь прямоуголника со сторонами " + a + " и " + b + "=" + area(a,b));
     }
+  public static void hello(String somebody){
+    System.out.println("Hello " + somebody + "!");
+  }
+  public static double area(double l) {
+    return  l * l;
+  }
+  public static double area(double a, double  b){
+    return a * b;
+  }
 }
 	
