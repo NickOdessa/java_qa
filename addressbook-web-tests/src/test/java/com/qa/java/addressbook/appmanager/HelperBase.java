8c1e48e3ebef37aa7cdbf1d56b7cf5756612f40d
NickOdessa <tkach.nickolay@gmail.com>
2016-12-08 04:21:43 +0200

    add class HelperBase

---
+++ addressbook-web-tests/src/test/java/com/qa/java/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,35 @@
+package com.qa.java.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by user on 08.12.2016.
+ */
+public class HelperBase {
+  protected FirefoxDriver wd;
+
+  public HelperBase(FirefoxDriver wd) {
+    this.wd=wd;
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
+
--- addressbook-web-tests/src/test/java/com/qa/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/com/qa/java/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by user on 07.12.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase{
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd=wd;
+    super(wd);
 
   }
   public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
+   click(By.linkText("home page"));
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+
+    click(By.linkText("groups"));
   }
 }
