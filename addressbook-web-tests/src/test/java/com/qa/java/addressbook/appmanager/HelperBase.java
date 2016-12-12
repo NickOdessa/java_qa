ee25d7fadeb39e41cbdc740a6292a02070c2e478
NickOdessa <tkach.nickolay@gmail.com>
2016-12-12 02:13:43 +0200

    Add WebDriver config

--- addressbook-web-tests/src/test/java/com/qa/java/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/com/qa/java/addressbook/appmanager/HelperBase.java
@@ -2,6 +2,7 @@ package com.qa.java.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,9 +10,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * Created by user on 08.12.2016.
  */
 public class HelperBase {
-  protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+  public HelperBase(WebDriver wd) {
     this.wd = wd;
   }
 
